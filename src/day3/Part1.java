88b82cfb872c50dbc047885422f0ea1925836128
ndemirel <ndemirel@bol.com>
2020-12-29 19:54:43 +0100

    gitignore and part2

--- src/day3/Part1.java
+++ src/day3/Part1.java
@@ -1,17 +1,20 @@
 package day3;
 
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Part1 {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws FileNotFoundException {
+        int counter = getCounter(3, 1);
+        System.out.println("Result: " + counter);
+    }
+
+    static int getCounter(int slopeX, int slopeY) throws FileNotFoundException {
         FileInputStream fis = new FileInputStream("src/day3/input.txt");
         Scanner sc = new Scanner(fis);
 
-        int slopeX = 3;
-        int slopeY = 1;
         int x = 0;
         int counter = 0;
         String line = sc.nextLine();
@@ -30,6 +33,6 @@ public class Part1 {
         }
 
         sc.close();
-        System.out.println("Result: " + counter);
+        return counter;
     }
 }
--- src/day3/Part2.java
+++ src/day3/Part2.java
@@ -1,4 +1,19 @@
 package day3;
 
+import java.io.FileNotFoundException;
+
+import static day3.Part1.getCounter;
+
 public class Part2 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        int result = getCounter(1, 1)
+                * getCounter(3, 1)
+                * getCounter(5, 1)
+                * getCounter(7, 1)
+                * getCounter(1, 2);
+
+        System.out.println(result);
+
+    }
 }
